8eae517ee9332ee8f6a4838c7548b18649cdd01d
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-08 01:03:51 -0500

    Add Map Page

--- Notes/MapPage.xaml.cs
+++ Notes/MapPage.xaml.cs
@@ -2,8 +2,22 @@ namespace Notes;
 
 public partial class MapPage : ContentPage
 {
-	public MapPage()
-	{
-		InitializeComponent();
-	}
+    int count = 0;
+
+    public MapPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
 }
