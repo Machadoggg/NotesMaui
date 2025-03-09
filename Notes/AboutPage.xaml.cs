352142f9c9732f9fae8d9baa345bf18e50507e16
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-08 22:18:24 -0500

    Edit About and Note pages

--- Notes/AboutPage.xaml.cs
+++ Notes/AboutPage.xaml.cs
@@ -2,14 +2,17 @@ namespace Notes;
 
 public partial class AboutPage : ContentPage
 {
-	public AboutPage()
-	{
-		InitializeComponent();
-	}
+    public AboutPage()
+    {
+        InitializeComponent();
+    }
 
-	private async void LearnMore_Clicked(object sender, EventArgs e)
-	{
-        // Navigate to the specified URL in the system browser.
-        await Launcher.Default.OpenAsync("https://aka.ms/maui");
-	}
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is About about)
+        {
+            // Navigate to the specified URL in the system browser.
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
+    }
 }
--- Notes/NotePage.xaml.cs
+++ Notes/NotePage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace Notes;
 
 public partial class NotePage : ContentPage
@@ -8,8 +10,28 @@ public partial class NotePage : ContentPage
     {
         InitializeComponent();
 
-        if (File.Exists(_fileName))
-            TextEditor.Text = File.ReadAllText(_fileName);
+        //if (File.Exists(_fileName))
+        //    TextEditor.Text = File.ReadAllText(_fileName);
+
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
+    }
+
+
+    private void LoadNote(string fileName)
+    {
+        Note noteModel = new Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
     }
 
     private void SaveButton_Clicked(object sender, EventArgs e)
