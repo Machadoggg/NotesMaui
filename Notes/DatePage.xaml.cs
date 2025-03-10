ac39934dfbb6a64d2b1d210974e1133f2ed7672c
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-09 19:44:02 -0500

    Implement DatePage

--- Notes/DatePage.xaml.cs
+++ Notes/DatePage.xaml.cs
@@ -1,3 +1,5 @@
+using Plugin.LocalNotification;
+
 namespace Notes;
 
 public partial class DatePage : ContentPage
@@ -6,4 +8,48 @@ public partial class DatePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnScheduleNotificationClicked(object sender, EventArgs e)
+    {
+        var selectedDate = datePicker.Date;
+        var selectedTime = timePicker.Time;
+
+        var notificationTime = new DateTime(
+            selectedDate.Year,
+            selectedDate.Month,
+            selectedDate.Day,
+            selectedTime.Hours,
+            selectedTime.Minutes,
+            selectedTime.Seconds
+        );
+
+        if (notificationTime > DateTime.Now)
+        {
+            ScheduleNotification(notificationTime);
+            DisplayAlert("Éxito", $"Notificación programada.{notificationTime}", "OK");
+        }
+        else
+        {
+            DisplayAlert("Error", "La fecha y hora deben ser futuras.", "OK");
+        }
+    }
+
+    private void ScheduleNotification(DateTime notificationTime)
+    {
+        var notification = new NotificationRequest
+        {
+            NotificationId = 1001,
+            Title = "App Name",
+            Subtitle = "Hello",
+            Description = "Description",
+            BadgeNumber = 42,
+            Schedule = new NotificationRequestSchedule
+            {
+                NotifyTime = notificationTime
+            }
+        };
+
+        // Programar la notificación
+        LocalNotificationCenter.Current.Show(notification);
+    }
 }
