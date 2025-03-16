761b61d398a7531597fffaeff8da97f8a72724fb
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-15 22:43:02 -0500

    Implement single note

--- Notes/Models/Note.cs
+++ Notes/Models/Note.cs
@@ -5,5 +5,35 @@
         public string Filename { get; set; } = default!;
         public string Text { get; set; } = default!;
         public DateTime Date { get; set; }
+
+
+        public Note()
+        {
+            Filename = $"{Path.GetRandomFileName()}.notes.txt";
+            Date = DateTime.Now;
+            Text = "";
+        }
+
+        public void Save() =>
+            File.WriteAllText(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename), Text);
+
+        public void Delete() => 
+            File.Delete(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename));
+
+        public static Note Load(string filename)
+        {
+            filename = System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
+
+            if (!File.Exists(filename))
+                throw new FileNotFoundException("Unable to find file on local storage.", filename);
+
+            return
+                new()
+                {
+                    Filename = Path.GetFileName(filename),
+                    Text = File.ReadAllText(filename),
+                    Date = DateTime.Now,
+                };  
+        }
     }
 }
